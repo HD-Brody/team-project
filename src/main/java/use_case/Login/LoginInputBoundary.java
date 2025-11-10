0f711df1df07a5abbd6fc482267409b15e4778e1
Pepper256 <splotchyscar@gmail.com>
2025-11-10 16:01:04 -0500

    add structure of login

--- src/main/java/use_case/Login/LoginInputBoundary.java
+++ src/main/java/use_case/Login/LoginInputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.Login;
 
-public class LoginInputBoundary {
+public interface LoginInputBoundary {
 }
--- src/main/java/use_case/Login/LoginOutputBoundary.java
+++ src/main/java/use_case/Login/LoginOutputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.Login;
 
-public class LoginOutputBoundary {
+public interface LoginOutputBoundary {
 }
