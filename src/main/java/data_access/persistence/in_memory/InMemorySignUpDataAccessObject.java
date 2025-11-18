a46df27eedfb314666c9d84490d5e05ab1cb79d7
Pepper256 <splotchyscar@gmail.com>
2025-11-17 23:11:37 -0500

    revert to unmerged

--- src/main/java/data_access/persistence/in_memory/InMemorySignUpDataAccessObject.java
+++ src/main/java/data_access/persistence/in_memory/InMemorySignUpDataAccessObject.java
@@ -4,7 +4,6 @@ import entity.User;
 import use_case.repository.SignUpRepository;
 
 import java.util.HashMap;
-import java.util.UUID;
 
 /**
  * Mock db for test purposes
@@ -12,24 +11,23 @@ import java.util.UUID;
 
 public class InMemorySignUpDataAccessObject implements SignUpRepository {
 
-    private final HashMap<String, String> usernameToUserIDDB;
+    private final HashMap<String, String> emailToUserIDDB;
     private final HashMap<String, String> db;
 
     public InMemorySignUpDataAccessObject() {
-        this.usernameToUserIDDB = new HashMap<String, String>();
+        this.emailToUserIDDB = new HashMap<String, String>();
         this.db = new HashMap<String, String>();
     }
 
     @Override
     public void saveUser(String userID, String name, String email, String timezone, String password) {
-        UUID uuid = UUID.randomUUID();
-        db.put(name, password);
-        usernameToUserIDDB.put(name, uuid.toString());
+        db.put(email, password);
+        emailToUserIDDB.put(email, userID);
     }
 
-    public User getUserByUsername(String username) {
-        if(db.containsKey(username))
-            return new User(usernameToUserIDDB.get(username), username, "123", "123");
+    public User getUserByEmail(String email) {
+        if(db.containsKey(email))
+            return new User(emailToUserIDDB.get(email), "123", email, "123");
         else
             return null;
     }
