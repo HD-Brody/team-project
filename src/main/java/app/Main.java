56936a83c2f589170f06c573a74d05b36610a2ac
Brody Honigman Deltoff <0707bhd@gmail.com>
2025-11-20 02:06:25 -0500

    Implement Main.java to make ui visible for my use case

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,20 @@
 package app;
 
+import javax.swing.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AppBuilder appBuilder = new AppBuilder();
+            JFrame application = appBuilder
+                    .addSyllabusUploadView()
+                    .addSyllabusUploadUseCase()
+                    .build();
+
+            application.pack();
+            application.setSize(700, 500);
+            application.setLocationRelativeTo(null);
+            application.setVisible(true);
+        });
+    }
 }
